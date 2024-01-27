4c6a37b23480041e8fdbb570ee0e87a11b2603e4
Diego Salas <dominicsc2hs@gmail.com>
2024-01-26 20:59:13 -0500

    feat: add User class and repository

--- src/main/java/io/getarrays/securecapita/repository/UserRepository.java
+++ src/main/java/io/getarrays/securecapita/repository/UserRepository.java
@@ -1,2 +1,17 @@
-package io.getarrays.securecapita.repository;public class UserRepository {
+package io.getarrays.securecapita.repository;
+
+import io.getarrays.securecapita.domain.User;
+
+import java.util.Collection;
+
+public interface UserRepository<T extends User>   {
+    /* Basic CRUD Operations */
+    T Create(T data);
+    Collection<T> list(int page, int pageSize);
+    T get(Long id);
+    T update(T data);
+    Boolean delete(Long id);
+
+    /* More Complex Operations */
 }
+
